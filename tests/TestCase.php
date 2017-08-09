50005c35f36a28a7785524174c3d264833e37d1d
Christian Schiffler <c.schiffler@cyberspectrum.de>
2017-08-09 22:17:10 +0200

    Add missing base test class

---
+++ tests/TestCase.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * This file is part of MetaModels/notelist.
+ *
+ * (c) 2017 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright  2017 The MetaModels team.
+ * @license    https://github.com/MetaModels/notelist/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+declare(strict_types = 1);
+
+namespace MetaModels\NoteList\Test;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    /**
+     * Preload the passed Contao classes.
+     *
+     * @param array $classes The classes to preload.
+     *
+     * @return void
+     */
+    protected function preloadContaoClasses(array $classes)
+    {
+        foreach ($classes as $class) {
+            if (!class_exists($class)) {
+                class_alias('Contao\\' . $class, $class);
+            }
+        }
+    }
+}
