9decfb7804b37362fbccdfc4dc251dc0a8370725
Ingolf Steinhardt <info@e-spin.de>
2017-11-09 16:17:14 +0100

    feature add contao session storage

---
+++ src/Storage/ContaoSessionAdapter.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * This file is part of MetaModels/notelist.
+ *
+ * (c) 2017 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2017 The MetaModels team.
+ * @license    https://github.com/MetaModels/notelist/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+declare(strict_types = 1);
+
+namespace MetaModels\NoteList\Storage;
+
+/**
+ * This class is the implementation of the Contao session storage.
+ */
+class ContaoSessionAdapter implements AdapterInterface
+{
+
+    /**
+     * The contao session.
+     *
+     * @var Session
+     */
+    private $session;
+
+    /**
+    * Initialize the object
+    */
+    public function __construct()
+    {
+        $this->session = \Session::getInstance();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getKey(string $key): array
+    {
+        if (null !== $this->session->get('metamodel_notelists')
+            && array_key_exists('metamodel_notelist_' . $key, $this->session->get('metamodel_notelists')) ) {
+            return (array) $this->session->get('metamodel_notelists')['metamodel_notelist_' . $key];
+        }
+
+        return [];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setKey(string $key, array $value)
+    {
+        $this->session->set('metamodel_notelists', ['metamodel_notelist_' . $key => $value]);
+    }
+}
