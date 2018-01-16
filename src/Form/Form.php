553bafe12798c13f7fa2ba579b2503f0ca41727a
Ingolf Steinhardt <info@e-spin.de>
2018-01-16 09:32:24 +0100

    add html attribute form id to notelist payload form

--- src/Form/Form.php
+++ src/Form/Form.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/notelist.
  *
- * (c) 2017 The MetaModels team.
+ * (c) 2017 - 2018 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -12,7 +12,8 @@
  *
  * @package    MetaModels
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @copyright  2017 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2017 - 2018 The MetaModels team.
  * @license    https://github.com/MetaModels/notelist/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
@@ -159,6 +160,7 @@ class Form
 
         $template = new Template('form');
         $template->setData([
+            'formId'     => 'nl_' . $this->noteList->getStorageKey() . '_f' . $item->get('id'),
             'hidden'     => '',
             'formSubmit' => $this->formId,
             'tableless'  => true,
