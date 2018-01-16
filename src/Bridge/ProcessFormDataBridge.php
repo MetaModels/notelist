e79091525c71a3d1f3252a292c4fad9fd0d8b3b4
Ingolf Steinhardt <info@e-spin.de>
2018-01-16 18:24:11 +0100

    delete author

--- src/Bridge/ProcessFormDataBridge.php
+++ src/Bridge/ProcessFormDataBridge.php
@@ -11,7 +11,6 @@
  * This project is provided in good faith and hope to be usable by anyone.
  *
  * @package    MetaModels
- * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Ingolf Steinhardt <info@e-spin.de>
  * @copyright  2017 - 2018 The MetaModels team.
  * @license    https://github.com/MetaModels/notelist/blob/master/LICENSE LGPL-3.0
