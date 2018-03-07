f29e4f3bb54b9bc69e47dc196ac90695f50168b5
Ingolf Steinhardt <info@e-spin.de>
2018-03-07 19:10:01 +0100

    Hotfix contao session adapter to save different notelists

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/notelist.
  *
- * (c) 2017 The MetaModels team.
+ * (c) 2017-2018 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -13,12 +13,14 @@
  * @package    MetaModels
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2017 The MetaModels team.
+ * @copyright  2017-2018 The MetaModels team.
  * @license    https://github.com/MetaModels/notelist/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
 
+
 $GLOBALS['TL_DCA']['tl_module']['palettes']['__selector__'][] = 'metamodel_add_notelist';
+
 $GLOBALS['TL_DCA']['tl_module']['palettes']['metamodel_list'] =
     str_replace(
         ';{template_legend:hide}',
@@ -26,9 +28,9 @@ $GLOBALS['TL_DCA']['tl_module']['palettes']['metamodel_list'] =
         $GLOBALS['TL_DCA']['tl_module']['palettes']['metamodel_list']
     );
 
-$GLOBALS['TL_DCA']['tl_module']['subpalettes']['metamodel_add_notelist'] = '';
+$GLOBALS['TL_DCA']['tl_module']['subpalettes']['metamodel_add_notelist'] = 'metamodel_notelist';
 
-$GLOBALS['TL_DCA']['tl_module']['fields']['add_notelist'] = [
+$GLOBALS['TL_DCA']['tl_module']['fields']['metamodel_add_notelist'] = [
     'label'     => &$GLOBALS['TL_LANG']['tl_module']['fields']['metamodel_add_notelist'],
     'inputType' => 'checkbox',
     'eval'      => [
