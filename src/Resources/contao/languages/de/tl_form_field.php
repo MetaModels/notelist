2aff40b4a83077409435e53727e7561e282cc1b1
Ingolf Steinhardt <info@e-spin.de>
2025-06-27 13:24:43 +0200

    Add DE translation

---
+++ src/Resources/contao/languages/de/tl_form_field.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * This file is part of MetaModels/notelist.
+ *
+ * (c) 2017-2025 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2017-2025 The MetaModels team.
+ * @license    https://github.com/MetaModels/notelist/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+declare(strict_types = 1);
+
+$GLOBALS['TL_LANG']['FFL']['metamodel_notelist'][0] = 'MetaModels Merkliste';
+$GLOBALS['TL_LANG']['FFL']['metamodel_notelist'][1] = 'MetaModels Merkliste';
+
+$GLOBALS['TL_LANG']['tl_form_field']['metamodel'][0]          = 'MetaModel';
+$GLOBALS['TL_LANG']['tl_form_field']['metamodel'][1]          = 'Wählen Sie das MetaModel aus, aus dem die Merkliste(n) geholt werden sollen.';
+$GLOBALS['TL_LANG']['tl_form_field']['metamodel_notelist'][0] = 'Merklisten';
+$GLOBALS['TL_LANG']['tl_form_field']['metamodel_notelist'][1] = 'Wählen Sie alle Merklisten aus, die hinzugefügt werden sollen.';
+
+$GLOBALS['TL_LANG']['tl_form_field']['metamodel_notelist_notelist'][0]                = 'Merkliste';
+$GLOBALS['TL_LANG']['tl_form_field']['metamodel_notelist_notelist'][1]                =
+    'Wählen Sie die Merkliste für die verschiedenen Ausgaben.';
+$GLOBALS['TL_LANG']['tl_form_field']['metamodel_notelist_rendersetting_frontend'][0]  = 'Formular-Rendereinstellungen';
+$GLOBALS['TL_LANG']['tl_form_field']['metamodel_notelist_rendersetting_frontend'][1]  =
+    'Wählen Sie die Rendereinstellungen für die Formularausgabe.';
+$GLOBALS['TL_LANG']['tl_form_field']['metamodel_notelist_rendersetting_email'][0]     = 'E-Mail-Rendereinstellungen';
+$GLOBALS['TL_LANG']['tl_form_field']['metamodel_notelist_rendersetting_email'][1]     =
+    'Wählen Sie die Rendereinstellungen für die E-Mail-Ausgabe.';
+$GLOBALS['TL_LANG']['tl_form_field']['metamodel_notelist_rendersetting_clearlist'][0] = 'Liste leeren';
+$GLOBALS['TL_LANG']['tl_form_field']['metamodel_notelist_rendersetting_clearlist'][1] =
+    'Merkliste nach Absenden des Formulars leeren.';
+
+$GLOBALS['TL_LANG']['tl_form_field']['metamodel_customTplEmail'][0] = 'E-Mail-Template';
+$GLOBALS['TL_LANG']['tl_form_field']['metamodel_customTplEmail'][1] =
+    'Wählen Sie ein E-Mail-Template - diese Vorlage enthält die Ausgabe der Rendereinstellungen für die E-Mail.';
